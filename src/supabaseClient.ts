1625a38d952a0e6f58d455f7e141c6d8bb4edb9a
Khushi Sharma <khushi.sharma@binarysemantics.com>
2025-07-11 22:39:08 +0530

    Initial deploy

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,8 +1,7 @@
 // src/supabaseClient.ts
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://jciuscvxrmxxjnxultnn.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpjaXVzY3Z4cm14eGpueHVsdG5uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE1Nzc1NTksImV4cCI6MjA2NzE1MzU1OX0.QB5BsKKFafSF_a9IsBa-aiydNSSkthhroWM7WL_J77U'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
-
